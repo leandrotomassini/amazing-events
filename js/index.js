36a68862fedd3ff84f1184be4ca9c5eb0ba7ffd8
Leandro C. Tomassini <leandroctomassini@gmail.com>
2023-09-01 12:11:29 -0300

    Correcciones tarea 2

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ function createCard(amazingEvent) {
   const card = document.createElement("div");
 
   card.innerHTML = `
-    <div class="card mb-2 mr-2 align-self-center bg-dark" style="width: 18rem;">
+    <div class="card mb-2 mr-2 align-self-center text-dark" style="width: 18rem;">
       <img src="${amazingEvent.image}" class="card-img-top" alt="${amazingEvent.name}">
       <div class="card-body d-flex flex-column justify-content-center">
         <h5 class="card-title">${amazingEvent.name}</h5>
         <h5 class="card-title">${amazingEvent.date}</h5>
         <p class="card-text">${amazingEvent.description}</p>
         <p>$${amazingEvent.price}</p>
-        <a href="#" class="btn btn-light">More details</a>
+        <a href="details.html" class="btn btn-dark">More details</a>
       </div>
     </div>
   `;
